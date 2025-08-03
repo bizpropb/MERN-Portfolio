1b3f9b943ad3cd6c78813b3ea069a92264b40a8c
PascalBossertOfficial <Pascal_bossert@outlook.com>
2025-08-03 21:14:55 +0200

    fix: hotreload client v. docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  
     port: 3000,       
-    strictPort: true, 
+    strictPort: true,
+    watch: {
+      usePolling: true,     // Add this
+      interval: 1000        // Add this
+    }
   }
 })
